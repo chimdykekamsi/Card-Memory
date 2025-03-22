09e2eead2aa86668f6f6266a537ddd67a79229b0
chimdykekamsi <ckamsi04@gmail.com>
2025-03-22 10:42:04 +0100

    feat(gameData): add history endpoint
    - Created a `/history` route to fetch saved
    game results
    - Implemented `getGameHistory` function
    to return game data sorted by date

--- backend/controllers/memoryController.js
+++ backend/controllers/memoryController.js
@@ -1,14 +1,15 @@
 const Save = require('../models/save');
+const {validateRequiredFields} = require('../utils/validation');
 
 exports.saveGameData = async (req, res) => {
     const { userID, gameDate, failed, difficulty, completed, timeTaken } = req.body;
 
-    console.log('Received data to save:', req.body); 
-
     try {
-       
-        if (!userID || !gameDate || difficulty === undefined || completed === undefined || timeTaken === undefined) {
-            return res.status(400).json({ message: 'Missing required fields' });
+        const requiredFields = ['userID', 'failed', 'difficulty', 'completed', 'timeTaken'];
+        const missingFields = validateRequiredFields(req.body, requiredFields);
+    
+        if (missingFields) {
+            return res.status(400).json({ message: `Missing required fields: ${missingFields.join(', ')}` });
         }
 
         const newSave = new Save({
@@ -25,5 +26,15 @@ exports.saveGameData = async (req, res) => {
     } catch (error) {
         console.error('Error saving game data:', error);
         res.status(500).json({ message: 'Error saving game data', error });
+    }
+};
+
+exports.getGameHistory = async (req, res) => {
+    try {
+        const gameHistory = await Save.find().sort({ gameDate: -1 }); // Sort by latest game
+        res.status(200).json(gameHistory);
+    } catch (error) {
+        console.error('Error fetching game history:', error);
+        res.status(500).json({ message: 'Error fetching game history', error });
     }
 };
